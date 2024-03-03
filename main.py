33526a87ffea0e97e14b6e25becee8a513c8245b
Ivan <103575282+DragonIvanRussia@users.noreply.github.com>
2024-03-03 19:39:28 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, redirect, request, make_response, session
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(port='80', host='192.168.1.51')
+    
